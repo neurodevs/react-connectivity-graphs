969953464ef48580e7458b56e38a0b3d45f68797
ericthecurious <hello@ericthecurious.com>
2025-02-15 03:57:37 -0600

    patch: create components dir

--- src/__tests__/LateralizedConnectivityGraph.test.ts
+++ src/__tests__/LateralizedConnectivityGraph.test.ts
@@ -6,7 +6,7 @@ import AbstractSpruceTest, {
 import LateralizedConnectivityGraph, {
     Graph,
     Network,
-} from '../LateralizedConnectivityGraph'
+} from '../components/LateralizedConnectivityGraph'
 
 export default class LateralizedConnectivityGraphTest extends AbstractSpruceTest {
     private static instance: Graph
