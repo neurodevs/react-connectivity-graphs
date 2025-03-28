2afafb95f0ad6605bcac7cc07b0fc21f2ce6d82e
eric-yates <hello@ericthecurious.com>
2025-03-28 14:50:51 -0600

    patch: renders div with expected className

---
+++ src/__tests__/GraphRenderer.test.tsx
@@ -0,0 +1,60 @@
+import { test, assert } from '@sprucelabs/test-utils'
+import { render } from '@testing-library/react'
+import React from 'react'
+import GraphRenderer from '../components/GraphRenderer'
+import AbstractDomTest from './AbstractDomTest'
+
+export default class GraphRendererTest extends AbstractDomTest {
+    private static element: React.ReactElement
+
+    protected static async beforeEach() {
+        await super.beforeEach()
+
+        this.element = this.createElement()
+    }
+
+    @test()
+    protected static async createsGraphRendererElement() {
+        assert.isTruthy(this.element, 'Should create a GraphRenderer element!')
+    }
+
+    @test()
+    protected static async createsElementWithExpectedType() {
+        assert.isEqual(
+            this.element.type,
+            GraphRenderer,
+            'Should create GraphRenderer element with expected type!'
+        )
+    }
+
+    @test()
+    protected static async rendersWithTopLevelDiv() {
+        const div = this.renderAndGetTopLevelDiv()
+
+        assert.isTruthy(div, 'Should render a div!')
+    }
+
+    @test()
+    protected static async rendersDivWithExpectedClassName() {
+        const div = this.renderAndGetTopLevelDiv()
+
+        assert.isEqual(
+            div.className,
+            'graph-renderer',
+            'Should render a div with class="graph-renderer"!'
+        )
+    }
+
+    private static renderAndGetTopLevelDiv() {
+        const { getByTestId } = this.render()
+        return getByTestId('graph-renderer')
+    }
+
+    private static createElement() {
+        return React.createElement(GraphRenderer)
+    }
+
+    private static render() {
+        return render(<GraphRenderer />)
+    }
+}
--- src/components/GraphRenderer.tsx
+++ src/components/GraphRenderer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const GraphRenderer: React.FC = () => {
-    return <></>
+    return <div className="graph-renderer" data-testid="graph-renderer"></div>
 }
 
 export default GraphRenderer
