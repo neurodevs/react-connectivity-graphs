ea15c2f121977b8ac19344f1d1f13f7f6143600d
eric-yates <hello@ericthecurious.com>
2025-03-28 23:54:32 -0600

    patch: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
+// LateralFlowGraph
+
 export { default as LateralFlowGraph } from './components/LateralFlowGraph'
 export * from './components/LateralFlowGraph'
+
+export { default as SpyLateralFlowGraph } from './testDoubles/SpyLateralFlowGraph'
+export * from './testDoubles/SpyLateralFlowGraph'
+
+// GraphRenderer
+
+export { default as GraphRenderer } from './components/GraphRenderer'
+export * from './components/GraphRenderer'
+
+// ReactFlow
+
+export { default as FakeReactFlow } from './testDoubles/FakeReactFlow'
+export * from './testDoubles/FakeReactFlow'
