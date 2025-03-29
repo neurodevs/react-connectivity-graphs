5ca18255a67ca0ef23660c734ae7fbeb36e63baa
eric-yates <hello@ericthecurious.com>
2025-03-28 22:38:19 -0600

    patch: create optional ReactFlowComponent argument for test doubles

--- src/components/GraphRenderer.tsx
+++ src/components/GraphRenderer.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactFlow from 'reactflow'
 
-export interface GraphRendererProps {}
+export interface GraphRendererProps {
+    ReactFlowComponent?: React.ComponentType<any>
+}
 
-const GraphRenderer: React.FC<GraphRendererProps> = () => {
+const GraphRenderer: React.FC<GraphRendererProps> = ({
+    ReactFlowComponent = ReactFlow,
+}) => {
     return (
         <div className="graph-renderer" data-testid="graph-renderer">
-            <ReactFlow />
+            <ReactFlowComponent />
         </div>
     )
 }
