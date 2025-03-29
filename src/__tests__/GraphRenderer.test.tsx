e8fe13e283269e4c6b7a26585f4d51a229438ae2
eric-yates <hello@ericthecurious.com>
2025-03-28 22:33:48 -0600

    patch: delete test that was accidentally committed

--- src/__tests__/GraphRenderer.test.tsx
+++ src/__tests__/GraphRenderer.test.tsx
@@ -52,17 +52,6 @@ export default class GraphRendererTest extends AbstractDomTest {
         assert.isTruthy(reactFlow, 'Should render ReactFlow as child of div!')
     }
 
-    @test()
-    protected static async passesNodesToReactFlow() {
-        const div = this.renderAndGetTopLevelDiv()
-        const reactFlow = div.querySelector('.react-flow')
-
-        assert.isTruthy(
-            reactFlow,
-            'Should render ReactFlow with nodes passed as props!'
-        )
-    }
-
     private static renderAndGetTopLevelDiv() {
         const { getByTestId } = this.render()
         return getByTestId('graph-renderer')
