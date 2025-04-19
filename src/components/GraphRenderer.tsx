a4a722f6282bc0b76bc83da86ed0dfc8afcdd1bd
eric-yates <hello@ericthecurious.com>
2025-04-18 23:16:02 -0600

    patch: delete unnecessary any

--- src/components/GraphRenderer.tsx
+++ src/components/GraphRenderer.tsx
@@ -11,7 +11,7 @@ export interface GraphRendererProps {
     onEdgeClick?: () => void
     onEdgeMouseEnter?: () => void
     onEdgeMouseLeave?: () => void
-    ReactFlowComponent?: React.ComponentType<any>
+    ReactFlowComponent?: React.ComponentType
     useMemoFn?: (factory: NodeTypesFactory, deps: DependencyList) => NodeTypes
 }
 
