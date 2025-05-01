ccfd39280be1335353dd9ddb4b9d5ffca2d090c8
eric-yates <hello@ericthecurious.com>
2025-04-30 21:41:52 -0600

    minor: setup React app

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,25 @@
+import { test, assert } from '@sprucelabs/test-utils'
+import React from 'react'
+import AbstractPackageTest from './AbstractPackageTest'
+import App from '../App'
+
+export default class AppTest extends AbstractPackageTest {
+    private static element: React.ReactElement
+
+    protected static async beforeEach() {
+        await super.beforeEach()
+
+        this.element = this.renderApp()
+    }
+
+    
+    @test()
+    protected static async rendersApp() {
+        assert.isTruthy(this.element, 'App failed to render!')
+    }
+
+    private static renderApp() {
+        return <App />
+    }
+
+}
