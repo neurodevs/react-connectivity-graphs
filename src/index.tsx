840365a3be6523f957a93653f1989b5866f4c48a
eric-yates <hello@ericthecurious.com>
2025-05-08 18:42:31 -0500

    patch: hydrate app root with graph data

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { GraphRendererProps } from './exports'
 import App from './ui/App'
 
-const root = ReactDOM.createRoot(document.getElementById('root')!)
+async function main() {
+    const res = await fetch('/graph-data.json')
+    const props: GraphRendererProps = await res.json()
 
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+    const root = ReactDOM.createRoot(document.getElementById('root')!)
+
+    root.render(
+        <React.StrictMode>
+            <App {...props} />
+        </React.StrictMode>
+    )
+}
+
+main().catch((error) => {
+    console.error('Error loading graph data:', error)
+    process.exit(1)
+})
