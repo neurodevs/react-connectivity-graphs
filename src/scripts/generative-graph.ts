4f2233a075d7369cc82431ebe0a136a123f190ba
eric-yates <hello@ericthecurious.com>
2025-07-02 16:36:01 -0700

    patch: make generative graph script

---
+++ src/scripts/generative-graph.ts
@@ -0,0 +1,28 @@
+const numNodes = 50
+
+export default {
+    nodes: Array.from({ length: numNodes }, (_, i) => {
+        const id = `${i + 1}`
+        return {
+            id,
+            label: `Node ${id}`,
+            abbreviation: `N${id}`,
+        }
+    }),
+    edges: Array.from({ length: numNodes - 1 }, (_, i) => {
+        const source = `${i + 1}`
+        const target = `${i + 2}`
+        const side =
+            i % 3 === 0
+                ? 'ipsilateral'
+                : i % 3 === 1
+                  ? 'contralateral'
+                  : 'bilateral'
+        return {
+            id: `e${source}-${target}`,
+            source,
+            target,
+            side,
+        }
+    }),
+}
