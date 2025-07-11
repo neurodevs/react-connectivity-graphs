162c2751580a16c3fc59573dce5340df6429caf8
eric-yates <hello@ericthecurious.com>
2025-07-11 16:36:46 -0700

    patch: render 9 side-by-side graphs for easier visual testing

--- src/__tests__/ui/App.test.tsx
+++ src/__tests__/ui/App.test.tsx
@@ -1,5 +1,5 @@
 import { test, assert } from '@sprucelabs/test-utils'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, within } from '@testing-library/react'
 import React from 'react'
 
 import FakeLateralFlowGraph, {
@@ -10,7 +10,11 @@ import App, { setGraphComponent } from '../../ui/App'
 import AbstractPackageTest from '../AbstractPackageTest'
 
 export default class AppTest extends AbstractPackageTest {
-    private static element: RenderResult
+    private static result: RenderResult
+
+    private static app: HTMLElement
+    private static container: HTMLElement
+    private static graphs: HTMLElement[]
 
     protected static async beforeEach() {
         await super.beforeEach()
@@ -18,12 +22,16 @@ export default class AppTest extends AbstractPackageTest {
         setGraphComponent(FakeLateralFlowGraph)
         resetFakeLateralFlowGraphProps()
 
-        this.element = this.render()
+        this.result = this.render()
+
+        this.app = this.result.getByTestId('app')
+        this.container = within(this.app).getByTestId('graphs-container')
+        this.graphs = within(this.container).getAllByTestId(/graph-\d+/)
     }
 
     @test()
-    protected static async rendersApp() {
-        assert.isTruthy(this.element, 'App failed to render!')
+    protected static async rendersResult() {
+        assert.isTruthy(this.result, 'App failed to render!')
     }
 
     @test()
@@ -34,6 +42,104 @@ export default class AppTest extends AbstractPackageTest {
             lastFakeLateralFlowGraphProps,
             'Should render GraphRenderer with props!'
         )
+    }
+
+    @test()
+    protected static async rendersApp() {
+        assert.isTruthy(this.app, 'App container not found!')
+    }
+
+    @test()
+    protected static async rendersAppContainerWithStyles() {
+        const expected = {
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: '0rem',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
+        }
+
+        const style = window.getComputedStyle(this.app)
+
+        const actual = {
+            display: style.display,
+            flexWrap: style.flexWrap,
+            gap: style.gap,
+            justifyContent: style.justifyContent,
+            alignItems: style.alignItems,
+        }
+
+        assert.isEqualDeep(
+            actual,
+            expected,
+            'App container should have correct styles!'
+        )
+    }
+
+    @test()
+    protected static async rendersGraphsInContainerForThreeFlexColumns() {
+        assert.isTruthy(this.container, 'Graphs container not found!')
+    }
+
+    @test()
+    protected static async rendersGraphsContainerWithStyles() {
+        const expected = {
+            display: 'flex',
+            flexWrap: 'wrap',
+            maxWidth: `calc(3 * 500px + 2 * 1rem)`,
+        }
+
+        const style = window.getComputedStyle(this.container)
+
+        const actual = {
+            display: style.display,
+            flexWrap: style.flexWrap,
+            maxWidth: style.maxWidth,
+        }
+
+        assert.isEqualDeep(
+            actual,
+            expected,
+            'Graphs container should have correct styles!'
+        )
+    }
+
+    @test()
+    protected static async rendersNineGraphs() {
+        assert.isLength(this.graphs, 9, 'Should render nine graphs!')
+    }
+
+    @test()
+    protected static async rendersGraphsWithStyles() {
+        this.graphs.forEach((graph, index) => {
+            const expected = {
+                height: '500px',
+                width: '500px',
+                border: '1px solid rgb(221, 221, 221)',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                margin: '0rem',
+            }
+
+            const style = window.getComputedStyle(graph)
+
+            const actual = {
+                height: style.height,
+                width: style.width,
+                border: style.border,
+                display: style.display,
+                alignItems: style.alignItems,
+                justifyContent: style.justifyContent,
+                margin: style.margin,
+            }
+
+            assert.isEqualDeep(
+                actual,
+                expected,
+                `Graph ${index + 1} should have correct styles!`
+            )
+        })
     }
 
     private static render() {
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,10 +1,53 @@
 import React from 'react'
+import generateGraph from '../scripts/generateGraph'
 import LateralFlowGraph, { LateralFlowGraphProps } from '../ui/LateralFlowGraph'
 
-const App: React.FC<LateralFlowGraphProps> = (props: LateralFlowGraphProps) => {
+const App: React.FC<LateralFlowGraphProps> = () => {
     return (
-        <div id="app" data-testid="app">
-            <GraphComponent {...props} />
+        <div
+            id="app"
+            data-testid="app"
+            style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                gap: '0rem',
+                justifyContent: 'center',
+                alignItems: 'flex-start',
+            }}
+        >
+            <div
+                data-testid="graphs-container"
+                style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    maxWidth: `calc(3 * 500px + 2 * 1rem)`,
+                }}
+            >
+                {Array.from({ length: 9 }, (_, i) => wrap(i + 1))}
+            </div>
+        </div>
+    )
+}
+
+function wrap(numNodes: number) {
+    return (
+        <div
+            key={numNodes}
+            style={{
+                height: 500,
+                width: 500,
+                border: '1px solid #ddd',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                margin: '0rem',
+            }}
+            data-testid={`graph-${numNodes}`}
+        >
+            <GraphComponent
+                {...(generateGraph(numNodes) as any)}
+                viewPadding={10}
+            />
         </div>
     )
 }
